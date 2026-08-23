8a52f0be33a61ef6631522f56313a3a73243c375
momenmohsen <momen.mohsen5000@gmail.com>
2026-08-23 08:01:49 +0300

    feat: update PricingPage component with new layout and styling

--- src/components/pricing/index.jsx
+++ src/components/pricing/index.jsx
@@ -1,7 +1,467 @@
 export default function LandingPage() {
   return (
     <>
-        <div>pricing</div>
+      <div class="relative isolate bg-gray-800 px-6 py-24 sm:py-32 lg:px-8">
+        <div class="mx-auto max-w-4xl text-center">
+          <h2 class="text-base/7 font-semibold text-white">Pricing</h2>
+
+          <p class="mt-2 text-5xl font-semibold tracking-tight text-balance text-white sm:text-6xl">
+            Choose the right plan for you
+          </p>
+        </div>
+
+        <p class="mx-auto mt-6 max-w-2xl text-center text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">
+          Choose an affordable plan that’s packed with the best features for
+          engaging your audience, creating customer loyalty, and driving sales.
+        </p>
+
+        <div
+          class="mx-auto mt-16 grid max-w-lg grid-cols-1
+         gap-6 sm:mt-20
+         lg:max-w-6xl lg:grid-cols-3
+         items-stretch"
+        >
+          <div
+            class="h-full flex flex-col
+         rounded-3xl bg-white p-8
+         ring-1 ring-white/10
+         border border-[#1e40af]
+         transition-all duration-300 ease-in-out
+         hover:z-10 hover:scale-105
+         hover:shadow-2xl
+         sm:p-10"
+          >
+            <h3 class="text-base/7 font-semibold text-gray-900">Hobby</h3>
+
+            <p class="mt-4 flex items-baseline gap-x-2">
+              <span class="text-5xl font-semibold tracking-tight text-gray-900">
+                $29
+              </span>
+
+              <span class="text-base text-gray-400">/month</span>
+            </p>
+
+            <p class="mt-6 text-base/7 text-gray-600">
+              The perfect plan if you're just getting started with our product.
+            </p>
+
+            <ul class="mt-8 space-y-3 text-sm/6 sm:mt-10">
+              <li class="flex gap-x-3">
+                <svg
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  class="h-6 w-5 flex-none text-[#1e40af]"
+                >
+                  <path
+                    d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                    clip-rule="evenodd"
+                    fill-rule="evenodd"
+                  />
+                </svg>
+                25 products
+              </li>
+
+              <li class="flex gap-x-3">
+                <svg
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  class="h-6 w-5 flex-none text-[#1e40af]"
+                >
+                  <path
+                    d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                    clip-rule="evenodd"
+                    fill-rule="evenodd"
+                  />
+                </svg>
+                Up to 10,000 subscribers
+              </li>
+
+              <li class="flex gap-x-3">
+                <svg
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  class="h-6 w-5 flex-none text-[#1e40af]"
+                >
+                  <path
+                    d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                    clip-rule="evenodd"
+                    fill-rule="evenodd"
+                  />
+                </svg>
+                Advanced analytics
+              </li>
+
+              <li class="flex gap-x-3">
+                <svg
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  class="h-6 w-5 flex-none text-[#1e40af]"
+                >
+                  <path
+                    d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                    clip-rule="evenodd"
+                    fill-rule="evenodd"
+                  />
+                </svg>
+                24-hour support response time
+              </li>
+            </ul>
+
+            <a
+              href="pricing-config.html"
+              class="mt-auto block rounded-md bg-[#1e40af] px-3.5 py-2.5
+         text-center text-sm font-semibold text-white
+         transition-colors duration-200
+         hover:bg-indigo-400"
+            >
+              Get started today
+            </a>
+          </div>
+
+          <div
+            class="relative rounded-3xl bg-white p-8 ring-2 ring-[#1e40af]
+             shadow-xl
+             transition-all duration-300 ease-in-out
+             hover:z-20 hover:scale-105
+             hover:shadow-2xl
+             sm:p-10"
+          >
+            <div
+              class="absolute -top-4 left-1/2 -translate-x-1/2
+               rounded-full bg-[#1e40af]
+               px-4 py-1 text-xs font-semibold text-white"
+            >
+              Most Popular
+            </div>
+
+            <h3 class="text-base/7 font-semibold text-gray-900">
+              Professional
+            </h3>
+
+            <p class="mt-4 flex items-baseline gap-x-2">
+              <span class="text-5xl font-semibold tracking-tight text-gray-900">
+                $59
+              </span>
+
+              <span class="text-base text-gray-400">/month</span>
+            </p>
+
+            <p class="mt-6 text-base/7 text-gray-600">
+              Everything you need to grow your business and reach more
+              customers.
+            </p>
+
+            <ul class="mt-8 space-y-3 text-sm/6 sm:mt-10">
+              <li class="flex gap-x-3">
+                <svg
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  class="h-6 w-5 flex-none text-[#1e40af]"
+                >
+                  <path
+                    d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                    clip-rule="evenodd"
+                    fill-rule="evenodd"
+                  />
+                </svg>
+                100 products
+              </li>
+
+              <li class="flex gap-x-3">
+                <svg
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  class="h-6 w-5 flex-none text-[#1e40af]"
+                >
+                  <path
+                    d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                    clip-rule="evenodd"
+                    fill-rule="evenodd"
+                  />
+                </svg>
+                Up to 50,000 subscribers
+              </li>
+
+              <li class="flex gap-x-3">
+                <svg
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  class="h-6 w-5 flex-none text-[#1e40af]"
+                >
+                  <path
+                    d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                    clip-rule="evenodd"
+                    fill-rule="evenodd"
+                  />
+                </svg>
+                Advanced analytics
+              </li>
+
+              <li class="flex gap-x-3">
+                <svg
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  class="h-6 w-5 flex-none text-[#1e40af]"
+                >
+                  <path
+                    d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                    clip-rule="evenodd"
+                    fill-rule="evenodd"
+                  />
+                </svg>
+                Priority support
+              </li>
+
+              <li class="flex gap-x-3">
+                <svg
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  class="h-6 w-5 flex-none text-[#1e40af]"
+                >
+                  <path
+                    d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                    clip-rule="evenodd"
+                    fill-rule="evenodd"
+                  />
+                </svg>
+                Marketing automations
+              </li>
+            </ul>
+
+            <a
+              href="pricing-config.html"
+              class="mt-8 block rounded-md bg-[#1e40af] px-3.5 py-2.5
+               text-center text-sm font-semibold text-white
+               transition-colors duration-200
+               hover:bg-indigo-400
+               sm:mt-10"
+            >
+              Get started today
+            </a>
+          </div>
+
+          <div
+            class="rounded-3xl bg-white p-8 ring-1 ring-white/10
+             border border-[#1e40af]
+             transition-all duration-300 ease-in-out
+             hover:z-10 hover:scale-105
+             hover:shadow-2xl
+             sm:p-10"
+          >
+            <h3 class="text-base/7 font-semibold text-gray-900">Enterprise</h3>
+
+            <p class="mt-4 flex items-baseline gap-x-2">
+              <span class="text-5xl font-semibold tracking-tight text-gray-900">
+                $99
+              </span>
+
+              <span class="text-base text-gray-400">/month</span>
+            </p>
+
+            <p class="mt-6 text-base/7 text-gray-600">
+              Dedicated support and infrastructure for your company.
+            </p>
+
+            <ul class="mt-8 space-y-3 text-sm/6 sm:mt-10">
+              <li class="flex gap-x-3">
+                <svg
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  class="h-6 w-5 flex-none text-[#1e40af]"
+                >
+                  <path
+                    d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                    clip-rule="evenodd"
+                    fill-rule="evenodd"
+                  />
+                </svg>
+                Unlimited products
+              </li>
+
+              <li class="flex gap-x-3">
+                <svg
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  class="h-6 w-5 flex-none text-[#1e40af]"
+                >
+                  <path
+                    d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                    clip-rule="evenodd"
+                    fill-rule="evenodd"
+                  />
+                </svg>
+                Unlimited subscribers
+              </li>
+
+              <li class="flex gap-x-3">
+                <svg
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  class="h-6 w-5 flex-none text-[#1e40af]"
+                >
+                  <path
+                    d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                    clip-rule="evenodd"
+                    fill-rule="evenodd"
+                  />
+                </svg>
+                Advanced analytics
+              </li>
+
+              <li class="flex gap-x-3">
+                <svg
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  class="h-6 w-5 flex-none text-[#1e40af]"
+                >
+                  <path
+                    d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                    clip-rule="evenodd"
+                    fill-rule="evenodd"
+                  />
+                </svg>
+                Dedicated support representative
+              </li>
+
+              <li class="flex gap-x-3">
+                <svg
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  class="h-6 w-5 flex-none text-[#1e40af]"
+                >
+                  <path
+                    d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                    clip-rule="evenodd"
+                    fill-rule="evenodd"
+                  />
+                </svg>
+                Custom integrations
+              </li>
+            </ul>
+
+            <a
+              href="pricing-config.html"
+              class="mt-8 block rounded-md bg-[#1e40af] px-3.5 py-2.5
+               text-center text-sm font-semibold text-white
+               transition-colors duration-200
+               hover:bg-indigo-400
+               sm:mt-10"
+            >
+              Get started today
+            </a>
+          </div>
+
+          <div
+            class="col-span-1 rounded-3xl bg-white p-8
+             ring-1 ring-white/10
+             border border-[#1e40af]
+             transition-all duration-300 ease-in-out
+             hover:z-10 hover:scale-105
+             hover:shadow-2xl
+             sm:p-10
+             lg:col-span-3"
+          >
+            <div
+              class="flex flex-col gap-8
+               lg:flex-row lg:items-center lg:justify-between"
+            >
+              <div class="lg:max-w-sm">
+                <h3 class="text-base/7 font-semibold text-gray-900">
+                  Business
+                </h3>
+
+                <p class="mt-4 flex items-baseline gap-x-2">
+                  <span class="text-5xl font-semibold tracking-tight text-gray-900">
+                    $199
+                  </span>
+
+                  <span class="text-base text-gray-400">/month</span>
+                </p>
+
+                <p class="mt-6 text-base/7 text-gray-600">
+                  Everything you need to grow and manage your business.
+                </p>
+              </div>
+
+              <ul
+                class="grid grid-cols-1 gap-3 text-sm/6
+                 sm:grid-cols-2 lg:flex-1 lg:max-w-2xl"
+              >
+                <li class="flex gap-x-3">
+                  <svg
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    class="h-6 w-5 flex-none text-[#1e40af]"
+                  >
+                    <path
+                      d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                      clip-rule="evenodd"
+                      fill-rule="evenodd"
+                    />
+                  </svg>
+                  Unlimited products
+                </li>
+
+                <li class="flex gap-x-3">
+                  <svg
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    class="h-6 w-5 flex-none text-[#1e40af]"
+                  >
+                    <path
+                      d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                      clip-rule="evenodd"
+                      fill-rule="evenodd"
+                    />
+                  </svg>
+                  Unlimited subscribers
+                </li>
+
+                <li class="flex gap-x-3">
+                  <svg
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    class="h-6 w-5 flex-none text-[#1e40af]"
+                  >
+                    <path
+                      d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                      clip-rule="evenodd"
+                      fill-rule="evenodd"
+                    />
+                  </svg>
+                  Advanced analytics
+                </li>
+
+                <li class="flex gap-x-3">
+                  <svg
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    class="h-6 w-5 flex-none text-[#1e40af]"
+                  >
+                    <path
+                      d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                      clip-rule="evenodd"
+                      fill-rule="evenodd"
+                    />
+                  </svg>
+                  Custom integrations
+                </li>
+              </ul>
+
+              <a
+                href="pricing-config.html"
+                class="rounded-md bg-[#1e40af] px-6 py-3
+                 text-center text-sm font-semibold text-white
+                 transition-colors duration-200
+                 hover:bg-indigo-400
+                 lg:shrink-0"
+              >
+                Get started today
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
